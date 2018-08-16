4bb0a156a2e41859fdbb9558c9a0a9b78886f294
John Vandenberg <jayvdb@gmail.com>
2018-08-16 15:21:44 +0700

    Skip unnecessary requirements
    Requirement markers should be evaluated, and
    requirements discarded when they are not applicable
    to the users environment.
    
    Fixes https://github.com/di/pytest-reqs/issues/16

--- test_reqs.py
+++ test_reqs.py
@@ -1,3 +1,5 @@
+import sys
+
 from distutils.version import LooseVersion
 from pip_api._installed_distributions import Distribution
 from pkg_resources import get_distribution
@@ -20,7 +22,17 @@ def mock_dist():
 
 
 @pytest.mark.parametrize(
-    "requirements", ["foo", "Foo", "foo==1.0", "foo>=1.0", "foo<=1.0", "# comment"]
+    "requirements",
+    [
+        "foo",
+        "Foo",
+        "foo==1.0",
+        "foo>=1.0",
+        "foo<=1.0",
+        "# comment",
+        'foo<=1.0; python_version>="1.0"',
+        'foo<=1.0; python_version<="%s"' % sys.version[:3],
+    ],
 )
 def test_existing_requirement(requirements, mock_dist, testdir, monkeypatch):
     testdir.makefile(".txt", requirements=requirements)
@@ -59,6 +71,14 @@ def test_canonicalization(requirements, dist, testdir, monkeypatch):
     assert "passed" in result.stdout.str()
 
 
+def test_unnecessary_requirement(mock_dist, testdir, monkeypatch):
+    testdir.makefile(".txt", requirements='foo;python_version<"%s"' % sys.version[:3])
+    monkeypatch.setattr("pytest_reqs.pip_api.installed_distributions", lambda: {})
+
+    result = testdir.runpytest("--reqs")
+    assert "passed" in result.stdout.str()
+
+
 def test_missing_requirement(mock_dist, testdir, monkeypatch):
     testdir.makefile(".txt", requirements="foo")
     monkeypatch.setattr("pytest_reqs.pip_api.installed_distributions", lambda: {})
@@ -68,7 +88,15 @@ def test_missing_requirement(mock_dist, testdir, monkeypatch):
     assert "passed" not in result.stdout.str()
 
 
-@pytest.mark.parametrize("requirements", ["foo==2.0", "foo>1.0", "foo<1.0"])
+@pytest.mark.parametrize(
+    "requirements",
+    [
+        "foo==2.0",
+        "foo>1.0",
+        "foo<1.0",
+        'foo==2.0; python_version=="%s"' % sys.version[:3],
+    ],
+)
 def test_wrong_version(requirements, mock_dist, testdir, monkeypatch):
     testdir.makefile(".txt", requirements=requirements)
     monkeypatch.setattr(
@@ -76,6 +104,7 @@ def test_wrong_version(requirements, mock_dist, testdir, monkeypatch):
         lambda: {mock_dist.name: mock_dist},
     )
 
+    name = requirements.split(";", 1)
     result = testdir.runpytest("--reqs")
     result.stdout.fnmatch_lines(
         ['*Distribution "foo" requires %s*' % (requirements), "*1 failed*"]
@@ -83,7 +112,10 @@ def test_wrong_version(requirements, mock_dist, testdir, monkeypatch):
     assert "passed" not in result.stdout.str()
 
 
-@pytest.mark.parametrize("requirements", ["foo=1.0", "foo=>1.0"])
+@pytest.mark.parametrize(
+    "requirements",
+    ["foo=1.0", "foo=>1.0", 'foo==2.0; python_version=="%s"' % sys.version[:3]],
+)
 def test_invalid_requirement(requirements, mock_dist, testdir, monkeypatch):
     testdir.makefile(".txt", requirements=requirements)
     monkeypatch.setattr(
