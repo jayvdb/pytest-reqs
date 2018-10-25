aa0f7a799ee30a3af7f3250a7cc700fb0d51bcbb
John Vandenberg <jayvdb@gmail.com>
2018-10-25 16:36:46 +0700

    Ignore case of package names
    Fixes https://github.com/di/pytest-reqs/issues/36

--- test_reqs.py
+++ test_reqs.py
@@ -17,8 +17,14 @@ def mock_dist():
     return stub(project_name='foo', version='1.0')
 
 
+@pytest.fixture
+def mock_dist_titlecase():
+    return stub(project_name='Foo', version='1.0')
+
+
 @pytest.mark.parametrize('requirements', [
     'foo',
+    'Foo',
     'foo==1.0',
     'foo>=1.0',
     'foo<=1.0',
@@ -33,6 +39,22 @@ def test_existing_requirement(requirements, mock_dist, testdir, monkeypatch):
     result = testdir.runpytest("--reqs")
     assert 'passed' in result.stdout.str()
 
+
+@pytest.mark.parametrize('requirements', [
+    'foo',
+    'Foo',
+    'foo==1.0',
+    'Foo==1.0',
+])
+def test_allow_titlecase(requirements, mock_dist_titlecase, testdir, monkeypatch):
+    testdir.makefile('.txt', requirements=requirements)
+    monkeypatch.setattr(
+        'pytest_reqs.pip_api.installed_distributions',
+        lambda: {mock_dist_titlecase.project_name: mock_dist_titlecase}
+    )
+
+    result = testdir.runpytest("--reqs")
+    assert 'passed' in result.stdout.str()
 
 def test_missing_requirement(mock_dist, testdir, monkeypatch):
     testdir.makefile('.txt', requirements='foo')
